c089518714c69bbfda2edd1d69bfc01ec6ea2423
Joseph <joseph@Mac-mini.local>
2020-08-13 12:35:30 -0700

    replacing contact video

--- src/pages/Contact/Intro.js
+++ src/pages/Contact/Intro.js
@@ -67,9 +67,9 @@ const Intro = () => {
     <Wrapper>
       <div ref={ref}></div>
 
-      {/* <Vid id="vid" autoPlay muted loop data-keepplaying>
+      <Vid id="vid" autoPlay muted loop data-keepplaying>
         <source id="mp4" src={ContactVideo} type="video/mp4" />
-      </Vid> */}
+      </Vid>
 
       <TextBox>
         <LargeTextWrap>
