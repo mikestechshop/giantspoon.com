2244ff6819308846591af356f922f7dc68306e0a
slong753 <spencer@spencerlong.com>
2021-01-26 15:50:31 -0500

    rm commented code

--- api/linkedin.js
+++ api/linkedin.js
@@ -8,8 +8,6 @@ var connection = mysql.createConnection({
 })
 //TODO: [ISC-190] change this password and put in env before going to prod
 module.exports = (req, res) => {
-    //     const { name = 'World' } = req.query
-    //     res.status(200).send(`Hello ${name}`)
     connection.connect((err) => {
         if (err) {
             console.log('Error connecting to Db')
@@ -17,14 +15,4 @@ module.exports = (req, res) => {
         }
         res.send('connected')
     })
-
-    // connection.query('SELECT * from test', function (error, results, fields) {
-    //     if (error) {
-    //         console.error('error connecting: ' + error.stack)
-    //         error.status(500).json({ stuff: 'here' })
-    //     }
-    //     console.log('connected as id ' + connection.threadId)
-    //     const thread = connection.threadId
-    //     fields.status(200).json({ morestuff: 'here' })
-    // })
 }
