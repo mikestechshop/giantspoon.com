f9794a6da721c7a38e86188e5cd0b40d3830aa59
slong753 <spencer@spencerlong.com>
2021-01-29 14:04:18 -0500

    xml validation

--- api/linkedin_call.js
+++ api/linkedin_call.js
@@ -35,8 +35,10 @@ module.exports = (req, res) => {
     if (parser.validate(xmlData) === true) {
         //optional (it'll return an object in case it's not valid)
         var jsonObj = parser.parse(xmlData, options)
-        console.log('validated')
+        console.log('valid xml')
         res.send('valid')
+    } else {
+        console.log('invalid xml')
     }
     connection.connect(function (err) {
         if (err) {
