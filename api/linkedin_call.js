de87a030fbece73dde99d9fb5ab7bbc9440438fc
Spencer Long <spencer@spencerlong.com>
2021-02-05 10:13:07 -0500

    city

--- api/linkedin_call.js
+++ api/linkedin_call.js
@@ -42,7 +42,7 @@ module.exports = (req, res) => {
         )
         .then(function (response) {
             console.log(response.data)
-            res.send('response: ' + response)
+            res.send('response: ' + response.data.city)
             xmlData = response
             if (parser.validate(xmlData) === 1) {
                 //optional (it'll return an object in case it's not valid)
