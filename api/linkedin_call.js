f93330a7448d9342e38d10ca292944a6ad89b0c7
slong753 <spencer@spencerlong.com>
2021-02-05 12:21:48 -0500

    regex

--- api/linkedin_call.js
+++ api/linkedin_call.js
@@ -42,7 +42,7 @@ module.exports = (req, res) => {
         )
         .then(function (response) {
             console.log(response.data)
-            xmlData = response.data.toString().replace('&nbsp;', '&amp;nbsp;')
+            xmlData = response.data.replace(/(&nbsp;)*/g, '')
             // var jsonObj = parser.parse(xmlData, options)
             // res.send('response: ' + jsonObj.city)
             if (parser.validate(xmlData) === 1) {
