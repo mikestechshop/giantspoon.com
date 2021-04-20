91b692f210f8ebde4f23b2823de5d9237292ff44
slong753 <spencer@spencerlong.com>
2021-04-20 17:16:18 -0400

    again

--- api/linkedin_call.js
+++ api/linkedin_call.js
@@ -58,7 +58,7 @@ module.exports = (req, res) => {
                 var tObj = parser.getTraversalObj(xmlData, options)
                 var jsonObj = parser.convertToJson(tObj, options)
                 var jsonObj = parser.parse(xmlData, options)
-                console.log(jsonObj)
+                console.log(jsonObj.job.job_title)
             }
             // console.log('fail')
             var tObj = parser.getTraversalObj(xmlData, options)
