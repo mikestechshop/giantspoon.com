91c8ccc4c148710a7c9cd40c9f1bc2312f3bed17
slong753 <spencer@spencerlong.com>
2021-04-20 16:40:34 -0400

    job

--- api/linkedin_call.js
+++ api/linkedin_call.js
@@ -39,7 +39,7 @@ module.exports = (req, res) => {
             }
         )
         .then(function (response) {
-            console.log(response.data.source.job)
+            console.log(response.data.job)
             xmlData = response.data
             // var jsonObj = parser.parse(xmlData, options)
             // res.send('response: ' + jsonObj.city)
